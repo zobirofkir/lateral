04dccd1957648c21148fa2b4c2bc140d492fa30e
csw <zobirofkir19@gmail.com>
2026-04-02 06:25:32 +0100

    create route

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'dashboard')->name('dashboard');
+Route::get('/', function() {
+    return inertia('WelcomePage');
 });
-
-require __DIR__.'/settings.php';
