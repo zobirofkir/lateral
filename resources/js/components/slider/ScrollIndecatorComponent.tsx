89161858a800742abed382bb6704903a6f91017d
csw <zobirofkir19@gmail.com>
2026-04-02 07:39:52 +0100

    change scroll indecator design

--- resources/js/components/slider/ScrollIndecatorComponent.tsx
+++ resources/js/components/slider/ScrollIndecatorComponent.tsx
@@ -5,12 +5,30 @@ const ScrollIndecatorComponent = () => {
   return (
     <>
       <motion.div
-        animate={{ y: [0, 10, 0] }}
-        transition={{ repeat: Infinity, duration: 2, ease: "easeInOut" }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 1 }}
         className="absolute bottom-8 left-1/2 -translate-x-1/2 z-20 hidden sm:block"
       >
-        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
-          <div className="w-1 h-2 bg-white rounded-full mt-2 animate-bounce" />
+        <div className="flex flex-col items-center gap-3">
+          {/* Animated line that extends */}
+          <div className="relative h-12 w-px bg-white/30 overflow-hidden">
+            <motion.div
+              animate={{ y: [-20, 20] }}
+              transition={{ repeat: Infinity, duration: 1.5, ease: "easeInOut" }}
+              className="absolute top-0 w-full h-1/2 bg-white/80"
+            />
+          </div>
+          
+          {/* Pulsing dot */}
+          <motion.div
+            animate={{ 
+              scale: [1, 1.2, 1],
+              opacity: [0.5, 1, 0.5]
+            }}
+            transition={{ repeat: Infinity, duration: 1.5 }}
+            className="w-1.5 h-1.5 bg-white/80 rounded-full"
+          />
         </div>
       </motion.div>
     </>
