264bbcff306fab7b71644e6485ad6f9dbffb9960
csw <zobirofkir19@gmail.com>
2026-04-02 16:17:34 +0100

    add documentation in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,37 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
+/**
+ * Welcome page
+ */
 Route::get('/', function() {
     return inertia('WelcomePage');
 });
 
+/**
+ * Discover Page
+ */
 Route::get('/discover-tanger', function() {
     return inertia('DiscoverTangerPage');
 });
 
+/**
+ * Rooms Page
+ */
 Route::get('/rooms', function() {
     return inertia('RoomsPage');
 });
 
+/**
+ * Bookings page
+ */
 Route::get('/bookings', function() {
     return inertia('BookingsPage');
 });
 
+/**
+ * Rules Page
+ */
 Route::get('/rules', function() {
     return inertia('RulesPage');
 });
